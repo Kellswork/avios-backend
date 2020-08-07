897fdbe395cbf7c5989765fc76c0e4ecdd12b772
kells <kellogbonna@gmail.com>
2020-08-07 15:02:11 +0100

    add parser

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import route from './routes'
+import bodyParser from 'body-parser';
+
 
 const app = express();
 const PORT = process.env.PORT || 3500;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) =>
   res.status(200).json({
