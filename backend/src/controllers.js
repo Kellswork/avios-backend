5dbdb6b5c21e7b1455b1e289a5445a8702774608
kells <kellogbonna@gmail.com>
2020-08-07 14:58:25 +0100

    delete product

--- backend/src/controllers.js
+++ backend/src/controllers.js
@@ -1,8 +1,8 @@
-import { getProduct } from './models/models';
+import { getProduct, createProduct, deleteProduct } from './models/models';
 import db from './models/db';
 export const getOneProduct = async (req, res) => {
   try {
-    const { rows } = await db.query(getProduct, [1]);
+    const { rows } = await db.query(getProduct, [req.params.id]);
     if (!rows[0]) {
       return res.status(404).json({
         error: 'no products with id found',
@@ -56,7 +56,7 @@ export const getOneProduct = async (req, res) => {
 
 export const addProduct = async (req, res) => {
   try {
-    const product = await db.query(products, [...req.body]);
+    const product = await db.query(createProduct, [...req.body]);
     res.status(201).json({
       data: {
         message: 'product added succesfully',
@@ -69,3 +69,27 @@ export const addProduct = async (req, res) => {
     });
   }
 };
+
+export const removeProduct = async (req, res) => {
+  console.log('ehhhh', req.params.id)
+  try {
+    const { id } = req.params;
+    let { rows } = await db.query(deleteProduct, [id]);
+    if (!rows[0]) {
+      return res.status(404).json({
+        status: 404,
+        error: "product not found"
+      });
+    }
+    rows = await db.query(deleteProduct, [id]);
+
+    res.status(200).json({
+      message: " product has been deleted successfully"
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: 500,
+      error: error.message
+    });
+  }
+}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
-import { getOneProduct } from './controllers'
+import { getOneProduct, addProduct, removeProduct } from './controllers'
 const router = Router();
 
 router.get('/:id', getOneProduct);
+
+router.post('/', addProduct);
+
+router.delete('/:id', removeProduct);
 
 export default router;
